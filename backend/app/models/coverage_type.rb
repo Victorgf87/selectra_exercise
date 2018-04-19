5b02395f022f12e76d578408dddf51fda9284106
Víctor M. Gonzalez Fuente <victor.gf87@gmail.com>
2018-04-20 01:59:19 +0200

    Improved coverage_type model

--- backend/app/models/coverage_type.rb
+++ backend/app/models/coverage_type.rb
@@ -1,2 +1,5 @@
+# This class represents a coverage for directions
 class CoverageType < ApplicationRecord
+  has_many :addresses # Maybe we will want to use this in the future.
+  validates_uniqueness_of :name
 end
