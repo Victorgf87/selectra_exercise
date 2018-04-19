719ce694f06b30f66f4cf23ab7784b80a816163f
Víctor M. Gonzalez Fuente <victor.gf87@gmail.com>
2018-04-20 01:58:09 +0200

    Improved address model

--- backend/app/models/address.rb
+++ backend/app/models/address.rb
@@ -1,2 +1,23 @@
+#This class represents an address with or without coverage in our Application
 class Address < ApplicationRecord
+  belongs_to :user
+  belongs_to :coverage_type, optional: true
+  validates_uniqueness_of :address
+  validates_presence_of :address
+
+
+  # Geocoder settings. Using Geocode to store latitude and longitude for future usage.
+  geocoded_by :address
+  # after_validation :geocode # auto-fetch coordinates. Disabled because of api limit
+
+
+  # Gets coverage name
+  def coverage_name
+    has_coverage? ? coverage_type.name : 'No coverage'
+  end
+
+  # Gets if has coverage
+  def has_coverage?
+    !coverage_type.blank?
+  end
 end
