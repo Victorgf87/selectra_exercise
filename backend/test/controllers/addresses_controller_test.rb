a0a034b3ed19f8d2107167bf057cbe559c075b00
Víctor M. Gonzalez Fuente <victor.gf87@gmail.com>
2018-04-20 04:12:19 +0200

    more tests

--- backend/test/controllers/addresses_controller_test.rb
+++ backend/test/controllers/addresses_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class AddressesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "home with registered user" do
+    sign_in users(:one)
+    get root_path
+    assert_response :success
+  end
+
+  test "home without registered user" do
+    get root_path
+    assert_redirected_to new_user_session_path
+  end
+
+  test "User should not be able to edit an address that doesn't own" do
+    sign_in users(:two)
+    get edit_address_path(addresses(:of_one))
+    assert_redirected_to root_path
+  end
+
+  test "User should be able to edit their own address" do
+    sign_in users(:one)
+    get edit_address_path(addresses(:of_one))
+    assert_response :success
+  end
+
+  # TODO: Create similar tests in  remaining actions
+  # TODO: Test API
 end
